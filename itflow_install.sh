45e382bb157aea1726ccb7654fc13c15566521c5
Johnny <johnny@metroid.830thorn.pittpc.com>
2023-07-07 17:17:21 -0400

    Removed UFW Firewall rule set as this is not compatible with Debian 12 and should be set at the hardware Firewall level, also added Debian 12 Compatibility, basic tidy as well

--- itflow_install.sh
+++ itflow_install.sh
@@ -1,27 +1,27 @@
-#Check if running on ubuntu 22.04
-#UBU22=$(grep 22.04 "/etc/"*"release")
-#if ! [[ $UBU22 ]]; then
-#  echo -ne "\033[0;31mThis script will only work on Ubuntu 22.04\e[0m\n"
-#  exit 1
-#fi
+# Check if running on Ubuntu 22.04 or Debian 12
+OS_Check=$(grep -E "22.04|Debian 12" "/etc/"*"release")
+if ! [[ $OS_Check ]]; then
+    echo -ne "\033[0;31mThis script will only work on Ubuntu 22.04 or Debian 12\e[0m\n"
+    exit 1
+fi
 
-#Enter domain
+# Enter domain
 while [[ $domain != *[.]*[.]* ]]
 do
-echo -ne "Enter your Domain${NC}: "
-read domain
+    echo -ne "Enter your Domain${NC}: "
+    read domain
 done
 
-#Generate mariadb password
+# Generate mariadb password
 mariadbpwd=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w 20 | head -n 1)
 
-#Generate Cron Key
+# Generate Cron Key
 cronkey=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w 20 | head -n 1)
 
-#Get the latest OS updates
+# Get the latest OS updates
 sudo apt-get update && sudo apt-get -y upgrade
 
-#Install apache2 & mariadb
+# Install apache2 & mariadb
 sudo apt-get install -y apache2
 sudo apt-get install -y mariadb-server
 sudo mariadb_secure_installation
@@ -32,20 +32,20 @@ sudo apt-get install -y git
 sudo a2enmod md
 sudo a2enmod ssl
 
-#Restart apache2
+# Restart apache2
 sudo systemctl restart apache2
 
-#Set firewall
-sudo ufw allow OpenSSH
-sudo ufw allow 'Apache Full'
-sudo ufw enable
+# Set firewall
+# sudo ufw allow OpenSSH
+# sudo ufw allow 'Apache Full'
+# sudo ufw enable
 
-#Create and set permissions on webroot
+# Create and set permissions on webroot
 mkdir /var/www/${domain}
 
 chown -R www-data:www-data /var/www/
 
-#Set Apache2 config file
+# Set Apache2 config file
 apache2="$(cat << EOF
 <VirtualHost *:80>
     ServerAdmin webmaster@localhost
@@ -62,34 +62,35 @@ sudo a2ensite ${domain}.conf
 sudo a2dissite 000-default.conf
 sudo systemctl restart apache2
 
-#Run certbot to get Free Lets Encrypt TLS Certificate
+# Run certbot to get Free Lets Encrypt TLS Certificate
 sudo certbot --apache --non-interactive --agree-tos --register-unsafely-without-email --domains ${domain}
 
-#Go to webroot
+# Go to webroot
 cd /var/www/${domain}
 
-#Clone ITFlow
+# Clone ITFlow
 git clone https://github.com/itflow-org/itflow.git .
 
-#Add Cronjobs
+# Add Cronjobs
 (crontab -l 2>/dev/null; echo "0 2 * * * sudo -u www-data php /var/www/${domain}/cron.php ${cronkey}") | crontab -
 (crontab -l 2>/dev/null; echo "* * * * * sudo -u www-data php /var/www/${domain}/cron_ticket_email_parser.php ${cronkey}") | crontab -
 (crontab -l 2>/dev/null; echo "* * * * * sudo -u www-data php /var/www/${domain}/cron_mail_queue.php ${cronkey}") | crontab -
 
-#Create temp file with the cronkey that setup will read and use
+# Create temp file with the cronkey that setup will read and use
 echo "<?php" > /var/www/${domain}/uploads/tmp/cronkey.php
 echo "\$itflow_install_script_generated_cronkey = \"${cronkey}\";" >> /var/www/${domain}/uploads/tmp/cronkey.php
 echo "?>" >> /var/www/${domain}/uploads/tmp/cronkey.php
 
-#Set permissions
+# Set permissions
 chown -R www-data:www-data /var/www/
 
-#Create MySQl DB
-    mysql -e "CREATE DATABASE itflow /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -e "CREATE USER itflow@localhost IDENTIFIED BY '${mariadbpwd}';"
-    mysql -e "GRANT ALL PRIVILEGES ON itflow.* TO 'itflow'@'localhost';"
-    mysql -e "FLUSH PRIVILEGES;"
+# Create MySQL DB
+mysql -e "CREATE DATABASE itflow /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+mysql -e "CREATE USER itflow@localhost IDENTIFIED BY '${mariadbpwd}';"
+mysql -e "GRANT ALL PRIVILEGES ON itflow.* TO 'itflow'@'localhost';"
+mysql -e "FLUSH PRIVILEGES;"
 
+# Print URL/DB/User info for end user to finish the setup process
 printf >&2 "Please go to https://${domain} to finish setting up ITFlow"
 printf >&2 "\n\n"
 printf >&2 "In database setup section enter the following:\n\n"
