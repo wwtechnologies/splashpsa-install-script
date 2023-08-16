57b9c1b05783713485312f186de3589adc5f4e5b
Johnny <johnny@pop-os.localdomain>
2023-08-16 11:14:28 -0400

    Restructure script to use function calls, improve error handling and easier readability, also added 500M Upload limits to the php.ini configuration file

--- itflow_install.sh
+++ itflow_install.sh
@@ -1,106 +1,123 @@
-# Check to make sure your root
-if [[ $EUID -ne 0 ]]; then
-  echo -ne "\033[0;31mThis script must be run as root.\e[0m\n"
-  exit 1
-fi
+#!/bin/bash
 
-# Check if running on Ubuntu 22.04 or Debian 12
-OS_Check=$(grep -E "22.04|12" "/etc/"*"release")
-if ! [[ $OS_Check ]]; then
-    echo -ne "\033[0;31mThis script will only work on Ubuntu 22.04 or Debian 12\e[0m\n"
-    exit 1
-fi
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        echo -e "\033[0;31mThis script must be run as root.\e[0m"
+        exit 1
+    fi
+}
 
-# Enter domain
-while [[ $domain != *[.]*[.]* ]]
-do
-    echo -ne "Enter your Fully Qualified Domain -- example (itflow.domain.com)${NC}: "
-    read domain
-done
+check_os() {
+    if ! grep -E "22.04|12" "/etc/"*"release" &>/dev/null; then
+        echo -e "\033[0;31mThis script will only work on Ubuntu 22.04 or Debian 12\e[0m"
+        exit 1
+    fi
+}
 
-# Generate mariadb password
-mariadbpwd=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w 20 | head -n 1)
+get_domain() {
+    while [[ $domain != *[.]*[.]* ]]; do
+        echo -ne "Enter your Fully Qualified Domain -- example (itflow.domain.com): "
+        read domain
+    done
+}
 
-# Generate Cron Key
-cronkey=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w 20 | head -n 1)
+generate_passwords() {
+    mariadbpwd=$(tr -dc 'A-Za-z0-9' < /dev/urandom | fold -w 20 | head -n 1)
+    cronkey=$(tr -dc 'A-Za-z0-9' < /dev/urandom | fold -w 20 | head -n 1)
+}
 
-# Get the latest OS updates
-apt-get update && apt-get -y upgrade
+install_packages() {
+    apt-get update && apt-get -y upgrade
+    apt-get install -y apache2 mariadb-server php libapache2-mod-php php-intl php-mysqli php-curl php-imap php-mailparse rewrite libapache2-mod-md certbot python3-certbot-apache git sudo
 
-# Install apache2 & mariadb
-apt-get install -y apache2
-apt-get install -y mariadb-server
-mariadb_secure_installation
-apt-get install -y php libapache2-mod-php php-intl php-mysqli php-curl php-imap php-mailparse 
-apt-get install -y rewrite libapache2-mod-md
-apt-get install -y certbot python3-certbot-apache
-apt-get install -y git
-apt-get install -y sudo
-a2enmod md
-a2enmod ssl
+    mariadb_secure_installation
 
-# Restart apache2
-systemctl restart apache2
+    a2enmod md
+    a2enmod ssl
+}
 
-# Set firewall
-# ufw allow OpenSSH
-# ufw allow 'Apache Full'
-# ufw enable
+modify_php_ini() {
+    local PHP_INI_PATH="/etc/php/php.ini"
+    sed -i 's/^;\?upload_max_filesize =.*/upload_max_filesize = 500M/' $PHP_INI_PATH
+    sed -i 's/^;\?post_max_size =.*/post_max_size = 500M/' $PHP_INI_PATH
+}
 
-# Create and set permissions on webroot
-mkdir /var/www/${domain}
+setup_firewall() {
+    # Uncomment the lines below if you want to enable UFW
+    # ufw allow OpenSSH
+    # ufw allow 'Apache Full'
+    # ufw enable
+}
 
-chown -R www-data:www-data /var/www/
+setup_webroot() {
+    mkdir -p /var/www/${domain}
+    chown -R www-data:www-data /var/www/
+}
 
-# Set Apache2 config file
-apache2="$(cat << EOF
-<VirtualHost *:80>
+setup_apache() {
+    apache2_conf="<VirtualHost *:80>
     ServerAdmin webmaster@localhost
     ServerName ${domain}
     DocumentRoot /var/www/${domain}
     ErrorLog /\${APACHE_LOG_DIR}/error.log
     CustomLog /\${APACHE_LOG_DIR}/access.log combined
-</VirtualHost>
-EOF
-)"
-echo "${apache2}" > /etc/apache2/sites-available/${domain}.conf
+</VirtualHost>"
 
-a2ensite ${domain}.conf
-a2dissite 000-default.conf
-systemctl restart apache2
+    echo "${apache2_conf}" > /etc/apache2/sites-available/${domain}.conf
 
-# Run certbot to get Free Lets Encrypt TLS Certificate
-certbot --apache --non-interactive --agree-tos --register-unsafely-without-email --domains ${domain}
+    a2ensite ${domain}.conf
+    a2dissite 000-default.conf
+    systemctl restart apache2
 
-# Go to webroot
-cd /var/www/${domain}
+    certbot --apache --non-interactive --agree-tos --register-unsafely-without-email --domains ${domain}
+}
 
-# Clone ITFlow
-git clone https://github.com/itflow-org/itflow.git .
+clone_itflow() {
+    git clone https://github.com/itflow-org/itflow.git /var/www/${domain}
+}
 
-# Add Cronjobs
-(crontab -l 2>/dev/null; echo "0 2 * * * sudo -u www-data php /var/www/${domain}/cron.php ${cronkey}") | crontab -
-(crontab -l 2>/dev/null; echo "* * * * * sudo -u www-data php /var/www/${domain}/cron_ticket_email_parser.php ${cronkey}") | crontab -
-(crontab -l 2>/dev/null; echo "* * * * * sudo -u www-data php /var/www/${domain}/cron_mail_queue.php ${cronkey}") | crontab -
+setup_cronjobs() {
+    (crontab -l 2>/dev/null; echo "0 2 * * * sudo -u www-data php /var/www/${domain}/cron.php ${cronkey}") | crontab -
+    (crontab -l 2>/dev/null; echo "* * * * * sudo -u www-data php /var/www/${domain}/cron_ticket_email_parser.php ${cronkey}") | crontab -
+    (crontab -l 2>/dev/null; echo "* * * * * sudo -u www-data php /var/www/${domain}/cron_mail_queue.php ${cronkey}") | crontab -
+}
 
-# Create temp file with the cronkey that setup will read and use
-echo "<?php" > /var/www/${domain}/uploads/tmp/cronkey.php
-echo "\$itflow_install_script_generated_cronkey = \"${cronkey}\";" >> /var/www/${domain}/uploads/tmp/cronkey.php
-echo "?>" >> /var/www/${domain}/uploads/tmp/cronkey.php
+generate_cronkey_file() {
+    mkdir -p /var/www/${domain}/uploads/tmp
+    echo "<?php" > /var/www/${domain}/uploads/tmp/cronkey.php
+    echo "\$itflow_install_script_generated_cronkey = \"${cronkey}\";" >> /var/www/${domain}/uploads/tmp/cronkey.php
+    echo "?>" >> /var/www/${domain}/uploads/tmp/cronkey.php
+    chown -R www-data:www-data /var/www/
+}
 
-# Set permissions
-chown -R www-data:www-data /var/www/
+setup_mysql() {
+    mysql -e "CREATE DATABASE itflow /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+    mysql -e "CREATE USER itflow@localhost IDENTIFIED BY '${mariadbpwd}';"
+    mysql -e "GRANT ALL PRIVILEGES ON itflow.* TO 'itflow'@'localhost';"
+    mysql -e "FLUSH PRIVILEGES;"
+}
 
-# Create MySQL DB
-mysql -e "CREATE DATABASE itflow /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-mysql -e "CREATE USER itflow@localhost IDENTIFIED BY '${mariadbpwd}';"
-mysql -e "GRANT ALL PRIVILEGES ON itflow.* TO 'itflow'@'localhost';"
-mysql -e "FLUSH PRIVILEGES;"
+print_final_instructions() {
+    echo "Please go to https://${domain} to finish setting up ITFlow"
+    echo ""
+    echo "In database setup section enter the following:"
+    echo "Database User: itflow"
+    echo "Database Name: itflow"
+    echo "Database Password: ${mariadbpwd}"
+}
 
-# Print URL/DB/User info for end user to finish the setup process
-printf >&2 "Please go to https://${domain} to finish setting up ITFlow"
-printf >&2 "\n\n"
-printf >&2 "In database setup section enter the following:\n\n"
-printf >&2 "Database User: itflow\n"
-printf >&2 "Database Name: itflow\n"
-printf >&2 "Database Password: ${mariadbpwd} \n\n"
+# Execution begins here
+check_root
+check_os
+get_domain
+generate_passwords
+install_packages
+modify_php_ini
+setup_firewall
+setup_webroot
+setup_apache
+clone_itflow
+setup_cronjobs
+generate_cronkey_file
+setup_mysql
+print_final_instructions
