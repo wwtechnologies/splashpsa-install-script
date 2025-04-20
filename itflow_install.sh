338e4c60a60efa7149dfa36d0f3c65cad34e7363
wwtechnologies <100593748+wwtechnologies@users.noreply.github.com>
2025-04-20 18:29:29 -0500

    Update itflow_install.sh

--- itflow_install.sh
+++ itflow_install.sh
@@ -220,7 +220,7 @@ clone_itflow() {
     log "Cloning CKTC-ITFlow"
     show_progress "9. Cloning ITFlow..."
     {
-        git clone https://github.com/defiantdevs/cktc-it.git /var/www/${domain} >> "$LOG_FILE" 2>&1
+        git clone https://github.com/wwtechnologies/splash-psa.git /var/www/${domain} >> "$LOG_FILE" 2>&1
         chown -R www-data:www-data /var/www/${domain}
     } & spin "Cloning ITFlow repository"
     echo -e "${GREEN}ITFlow cloned to /var/www/${domain}.${NC}"
